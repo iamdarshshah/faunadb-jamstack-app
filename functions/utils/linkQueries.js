7b26e9d694e14262960359c761614c3ff0ecea51
iamdarshshah <imdarshshah@gmail.com>
2020-07-08 17:18:49 +0530

    createLinks - adding record to faunaDB ✍️

--- functions/utils/linkQueries.js
+++ functions/utils/linkQueries.js
@@ -12,6 +12,19 @@ const GET_LINKS = `
         }
       `;
 
+const CREATE_LINK = `
+    mutation ($name: String!, $url: String!, $description: String!) {
+        createLink(data: {name: $name, url: $url, description: $description, archived: false}) {
+            name
+            url
+            description
+            archived
+            _id
+        }
+    }
+`;
+
 module.exports = {
   GET_LINKS,
+  CREATE_LINK,
 };
